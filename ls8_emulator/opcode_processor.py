afc20fb82b942c38d04a3d9c9fd30fc94619b461
Miikis <Miikis@users.noreply.github.com>
2019-11-15 13:54:57 -0800

    Implement JEQ and JNE
    Signed-off-by: Miikis <Miikis@users.noreply.github.com>

--- ls8_emulator/opcode_processor.py
+++ ls8_emulator/opcode_processor.py
@@ -6,7 +6,9 @@ class OpcodeProcessor():
             0b01000111: self.print_data,
             0b00000001: self.halt_cpu,
             0b10100010: self.multiply,
-            0b10100111: self.compare
+            0b10100111: self.compare,
+            0b01010101: self.jump_if_equal,
+            0b01010110: self.jump_if_not_equal
         }
 
     def load_data_immediately(self):
@@ -38,13 +40,36 @@ class OpcodeProcessor():
 
         # TODO: Use bitshifting instead
         if a < b:
-            self.cpu.current_flags = 100
+            self.cpu.current_flags = 0b100
         elif a > b:
-            self.cpu.current_flags = 10
+            self.cpu.current_flags = 0b010
         else:
-            self.cpu.current_flags = 1
+            self.cpu.current_flags = 0b001
 
         return 2
+
+    def jump_if_equal(self):
+        current_count = self.cpu.program_counter
+        is_equal_set = self.cpu.current_flags == 0b001
+
+        if not is_equal_set:
+            return 1
+
+        register = self.cpu.ram.read(current_count + 1)
+        self.cpu.program_counter = self.cpu.main_register[register]
+        return -1
+
+    def jump_if_not_equal(self):
+        current_count = self.cpu.program_counter
+        current_flags = self.cpu.current_flags
+        is_equal_set = current_flags == 0b001
+
+        if is_equal_set:
+            return 1
+
+        register = self.cpu.ram.read(current_count + 1)
+        self.cpu.program_counter = self.cpu.main_register[register]
+        return -1
 
     def halt_cpu(self):
         self.cpu.is_running = False
