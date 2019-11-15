dd766b806dfd55a6d6a908afbcfe2a36afb08478
Miikis <Miikis@users.noreply.github.com>
2019-11-15 09:05:59 -0800

    Print parsed lines_of_code from ls8 file as a list
    Signed-off-by: Miikis <Miikis@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from utils import InputParser
-
+from ls8_emulator import LS8Parser
 
 if __name__ == "__main__":
     input_parser = InputParser()
-    print(input_parser)
+    filename = input_parser[0]
+
+    ls8_parser = LS8Parser()
+    ls8_parser.parse(filename)
+
+    print(ls8_parser.lines_of_code)
