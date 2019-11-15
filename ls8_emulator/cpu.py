415cb9553819295ad407dcd58ce4ae7d3ccefe08
Miikis <Miikis@users.noreply.github.com>
2019-11-15 09:41:51 -0800

    Setup registers
    Signed-off-by: Miikis <Miikis@users.noreply.github.com>

--- ls8_emulator/cpu.py
+++ ls8_emulator/cpu.py
@@ -3,6 +3,15 @@ from .ram import RAM
 
 class CPU():
     def __init__(self):
+        self.is_running = False
+
+        # Registers
+        self.program_counter = 0
+        self.current_mem_address = 0
+        self.current_mem_data = 0
+        self.current_instruction = 0
+        self.current_flags = 0
+
         self.ram = RAM()
 
     def load_program(self, program=[]):
@@ -13,5 +22,12 @@ class CPU():
             address += 1
 
     def start(self):
-        print("Started from the top!")
-        print(self.ram)
+        self.is_running = True
+
+        while self.is_running:
+            if self.program_counter > len(self.ram) - 1:
+                self.is_running = False
+                continue
+
+            print(self.ram.read(self.program_counter))
+            self.program_counter += 1
